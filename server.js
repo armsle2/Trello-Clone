ff9bc797cd321e5db224abb9915b609193afe46a
LA The Fly Rebel <flyrebel@server.theflyrebel.com>
2018-01-13 14:23:25 -0500

    connected handlebars to html-route and config server.js file appropriately

--- server.js
+++ server.js
@@ -5,15 +5,19 @@ const db = require('./models');
 
 const app = express();
 
-let PORT = process.env.PORT||3000;
 
+
+let PORT = process.env.PORT||3000;
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json({type: "application/vnd.api+json"}));
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars')
 // require('./routes/api-routes.js')(app);
-// require('./routes/html-routes.js')(app);
+require('./routes/html-routes.js')(app);
 
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
